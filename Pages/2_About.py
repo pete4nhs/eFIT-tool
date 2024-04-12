89d6d1326008eab8529cd00b5a73a6664d999eb3
pete4nhs <165719339+pete4nhs@users.noreply.github.com>
2024-04-12 11:24:18 +0100

    Add files via upload

---
+++ Pages/2_About.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+# This is to add in a title for our web app's page
+st.title('FAQs')
+st.write('**Q:** What and who is this tool for?')
+st.write('**A:** This tool is a calculator for ICB directors/managers ICB financial teams in England who receive **extra funding** '
+         'for GP practices and wish to split the money based on **inequalities** and/or **local data**. ')
+st.write('')
+st.write('')
+st.write('')
+st.write("**Q:** Is there a way to calculate the allocation just based on reigistered population size (rather than weighted population)?")
+st.write("**A:** Yes! If from the sidebar, you leave all the indicators boxes unselected (reading _'Choose an option'_), the allocation will be calculated solely based on the population size selected from the demographics section")
+st.write('')
+st.write('')
+st.write('')
+st.write("**Q:** When I try to upload my data from excel it gives the error 'UnicodeDecodeError: 'utf-8' codec can't decode bytes in position 15-16: invalid continuation byte'")
+st.write("**A:** You can only upload files in a '.csv' format. Hence, with your file open with excel, re-save the document in .csv (save as :arrow_right: format: 'csv')")
+
+st.write('')
+st.title('Methodology')
+st.write('For each indicator we have applied the following formula:')
+# ADD equation (for 'single' indicator')
+st.image('single_equation.png', width=200)
+st.write('Hence, given that you can assign different weights to each indicator, the resulting equation is the following:')
+# ADD equation (for all indicators')
+st.image('longer_equation.png', width=300)
+st.write('where:')
+st.image('where.png', width=500)
+
+st.write('')
+st.title('Data sources')
+# Messages
+st.write('Indicators data **for 2022/23**, including IMD scores (2019)* and prevalence (QOF) data, was extracted on 26/3/2024 from fingertips ( https://fingertips.phe.org.uk/ ).')
+st.write('*Where new GP practices opened or merged/demerged (e.g. Cambridgeshire and Peterborough ICB and Nottingham and Nottinghamshire ICB), '
+         'we have calculated their new IMD.')
+
+st.write('Weighted population data is for 2024/25 and was extracted from: J-Overall weighted populations by ICB and GP practice 2023/24 to 2024/25 at NHS England >> Supporting spreadsheets for allocations 2023/24 to 2024/25'
+         'available at: https://www.england.nhs.uk/publication/supporting-spreadsheets-for-allocations-2023-24-to-2024-25/ ')
+st.write('GP practices population size data is as of March 2024 and were extracted from NHS Digital at: https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice')
+
+
+
+st.write('')
+st.write('')
+st.write('')
+st.write(' **To report any bug, feedback or special requests please email: peter.saiu@nhs.net** ')
